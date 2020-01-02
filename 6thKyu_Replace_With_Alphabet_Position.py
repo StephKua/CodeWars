4462d3af81ee65cd79585687b40125c22d3524ff
Steph <stephenkkz97@gmail.com>
2020-01-02 23:12:59 +0800

    improved

--- 6thKyu_Replace_With_Alphabet_Position.py
+++ 6thKyu_Replace_With_Alphabet_Position.py
@@ -11,10 +11,12 @@
 # alphabet_position("The sunset sets at twelve o' clock.")
 # Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" as a string.
 
-from string import ascii_lowercase
-from itertools import count
-
 def alphabet_position(text):
-    letters = dict(zip(ascii_lowercase, count(1)))
-    ans = [str(letters[t]) for t in text.lower() if t in letters]
-    return " ".join(ans)
+    dict = {'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 
+    'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j':'10',
+    'k': '11', 'l': '12', 'm': '13', 'n': '14', 'o': '15',
+    'p': '16', 'q': '17', 'r': '18', 's': '19', 't': '20',
+    'u': '21', 'v': '22', 'w': '23', 'x': '24', 'y': '25', 
+    'z': '26'
+    }
+    return " ".join([dict[a] for a in text.lower() if a in dict.keys()])
